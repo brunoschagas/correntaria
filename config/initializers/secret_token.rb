79a394be41d837a1e03cd851a7dc4ab234379b1c
Bruno Santos Chagas <brunoprimo@gmail.com>
2013-07-17 19:55:58 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Correntaria::Application.config.secret_token = '8660f85b4dc51e13ca6d79b63333dad1c8a3784df119b98e6069b78e26cde23938edd6f50af8105d701ce680b9a70d99bd53ea84e4333489c8892d9ce7868c9e'
